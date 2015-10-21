6dfabd6d520f2f0c93a06f59c5e94ecbae1574f9
Marco Ferrari <ferrari.marco@gmail.com>
2015-10-21 19:13:12 +0200

    Enabled REST URLs automatic configuration

--- oshi_rest_server/oshi_rest_server/urls.py
+++ oshi_rest_server/oshi_rest_server/urls.py
@@ -5,8 +5,11 @@ from rest_framework import routers
 
 admin.autodiscover()
 
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+
 urlpatterns = patterns('',
-                       url(r'^', include(routers.urls)),
+                       url(r'^', include(router.urls)),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
