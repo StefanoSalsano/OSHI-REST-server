9ad5987b7838ad2964aa506a22bf8454be334ae0
Marco Ferrari <ferrari.marco@gmail.com>
2015-10-22 20:05:10 +0200

    Removed x-grid and added line_width params

--- oshi_rest_server/rrdgraph_server/rrdgraphgenerator.py
+++ oshi_rest_server/rrdgraph_server/rrdgraphgenerator.py
@@ -5,15 +5,15 @@ from pyrrd.graph import DEF, LINE, Graph
 
 
 def get_rrdgraph(rrd_file_name, data_source_name, start_time='N', end_time='N', width=None, height=None,
-                 line_color='#FF0000'):
+                 line_color='#FF0000', line_width=1):
 
     rrd_input = rrd_file_name
     if path.isfile(rrd_input) is not True:
         raise RrdDoesNotExist("{} not available".format(rrd_file_name))
     output_file = '/tmp/rrd_graph_{0}.png'.format(int(time.time() * 1000))
     def1 = DEF(rrdfile=rrd_file_name, vname=data_source_name, dsName=data_source_name)
-    line1 = LINE(value=data_source_name, color=line_color)
-    g = Graph(output_file, start=start_time, end=end_time, x_grid='MINUTE:10:HOUR:1:MINUTE:120:0:%R',
+    line1 = LINE(value=data_source_name, color=line_color, width=line_width)
+    g = Graph(output_file, start=start_time, end=end_time,
               lower_limit=0, imgformat='PNG', width=width, height=height)
     g.data.extend([def1, line1])
     g.write()
--- tests/rrdgraph_server/test_rrdgraphgenerator.py
+++ tests/rrdgraph_server/test_rrdgraphgenerator.py
@@ -1,9 +1,10 @@
-# TODO: write a test to check if the generated graph is equal to expected_graph.png.
 # expected_graph.png has been generated with the following rrdgraph command:
 # rrdtool graph expected_graph.png                                 \
 #         --start 920804400 --end 920808000               \
 #         DEF:myspeed=test.rrd:speed:AVERAGE              \
 #         LINE2:myspeed#FF0000
+from PIL import Image
+import ImageChops
 import os
 from oshi_rest_server.rrdgraph_server.rrdgraphgenerator import get_rrdgraph
 
@@ -13,5 +14,12 @@ class TestRrdGraphGenerator(object):
         print("Testing rrdgraph generator (get_rrdgraph)")
         test_rrd_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test.rrd')
         print("Test RRD file path: {}".format(test_rrd_path))
-        rrd_output = get_rrdgraph(test_rrd_path, 'speed', start_time=920804400, end_time=920808000)
-        print("Log output: {}".format(rrd_output))
+        rrd_output = get_rrdgraph(test_rrd_path, 'speed', start_time=920804400, end_time=920808000, line_width=2)
+        print("RRD Graph output: {}".format(rrd_output))
+        result = Image.open(rrd_output)
+        expected = Image.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'expected_graph.png'))
+        assert _are_images_equal(result, expected) is True
+
+
+def _are_images_equal(im1, im2):
+    return ImageChops.difference(im1, im2).getbbox() is None
