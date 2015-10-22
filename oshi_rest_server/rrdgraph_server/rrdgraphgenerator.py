02c503b6fe81da618f964228f55778472e951b73
Marco Ferrari <ferrari.marco@gmail.com>
2015-10-22 19:42:57 +0200

    Modified RrdGraphImplementation to use PyRRD.

--- oshi_rest_server/rrdgraph_server/rrdgraphgenerator.py
+++ oshi_rest_server/rrdgraph_server/rrdgraphgenerator.py
@@ -1,19 +1,14 @@
-from sys import path
+from os import path
 import time
 import rrdtool
 from oshi_rest_server.rrdgraph_server.exceptions import RrdDoesNotExist
+from pyrrd.graph import DEF, CDEF, VDEF, LINE, AREA, GPRINT, Graph
 
 
-def get_rrdgraph(**_kwargs):
-    rrd_file_name = _kwargs.get('rrd_file_name', '')
-    data_source_name = _kwargs.get('data_source_name', '')
-    start_time = _kwargs.get('start_time', 'N')
-    end_time = _kwargs.get('end_time', 'N')
-
-    # Graph config
+def get_rrdgraph(rrd_file_name, data_source_name, start_time='N', end_time='N'):
     width = '785'
     height = '120'
-    color = '0000FF'
+    line_color = '#FF0000'
 
     rrd_input = rrd_file_name
     if path.isfile(rrd_input) is not True:
@@ -21,12 +16,19 @@ def get_rrdgraph(**_kwargs):
 
     output_file = '/tmp/rrd_graph_{0}.png'.format(int(time.time() * 1000))
 
-    data_definition = "DEF:vname={0}:{1}:AVERAGE".format(rrd_input, data_source_name)
-    line_definition = "LINE1:vname#{0}:{1}".format(color, data_source_name)
-    rrdtool.graph(output_file, '--start', str(start_time), '--end', str(end_time),
-                  '-a', 'PNG',
-                  '--lower-limit', '0',
-                  '-w', str(width), '-h', str(height),
-                  '--x-grid', 'MINUTE:10:HOUR:1:MINUTE:120:0:%R',
-                  data_definition, line_definition)
+    # data_definition = "DEF:vname={0}:{1}:AVERAGE".format(rrd_input, data_source_name)
+    # line_definition = "LINE1:vname#{0}:{1}".format(line_color, data_source_name)
+    # rrdtool.graph(output_file, '--start', str(start_time), '--end', str(end_time),
+    #               '-a', 'PNG',
+    #               '--lower-limit', '0',
+    #               '-w', str(width), '-h', str(height),
+    #               '--x-grid', 'MINUTE:10:HOUR:1:MINUTE:120:0:%R',
+    #               data_definition, line_definition)
+
+    def1 = DEF(rrdfile=rrd_file_name, vname=data_source_name, dsName=data_source_name)
+    line1 = LINE(value=data_source_name, color=line_color)
+    g = Graph(output_file, start=start_time, end=end_time, x_grid='MINUTE:10:HOUR:1:MINUTE:120:0:%R',
+              lower_limit=0, imgformat='PNG', width=width, height=height)
+    g.data.extend([def1, line1])
+    g.write()
     return output_file
--- tests/rrdgraph_server/test_rrdgraphgenerator.py
+++ tests/rrdgraph_server/test_rrdgraphgenerator.py
@@ -4,3 +4,14 @@
 #         --start 920804400 --end 920808000               \
 #         DEF:myspeed=test.rrd:speed:AVERAGE              \
 #         LINE2:myspeed#FF0000
+import os
+from oshi_rest_server.rrdgraph_server.rrdgraphgenerator import get_rrdgraph
+
+
+class TestRrdGraphGenerator(object):
+    def test_get_rrdgraph(self):
+        print("Testing rrdgraph generator (get_rrdgraph)")
+        test_rrd_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test.rrd')
+        print("Test RRD file path: {}".format(test_rrd_path))
+        rrd_output = get_rrdgraph(test_rrd_path, 'speed', start_time=920804400, end_time=920808000)
+        print("Log output: {}".format(rrd_output))
