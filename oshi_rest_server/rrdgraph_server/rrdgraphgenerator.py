4c9e9d4b5d05af9e7acae460a1cc034903630980
Marco Ferrari <ferrari.marco@gmail.com>
2015-10-28 19:02:57 +0100

    Added graph_title parameter to rrdgraphgenerator

--- oshi_rest_server/rrdgraph_server/rrdgraphgenerator.py
+++ oshi_rest_server/rrdgraph_server/rrdgraphgenerator.py
@@ -1,10 +1,10 @@
 from os import path
 import time
-from oshi_rest_server.rrdgraph_server.exceptions import RrdDoesNotExist
+from rrdgraph_server.exceptions import RrdDoesNotExist
 from pyrrd.graph import DEF, LINE, Graph
 
 
-def get_rrdgraph(rrd_file_name, data_source_name, start_time='N', end_time='N', width=None, height=None,
+def get_rrdgraph(rrd_file_name, data_source_name, graph_title=None, start_time='N', end_time='N', width=None, height=None,
                  line_color='#FF0000', line_width=1):
 
     rrd_input = rrd_file_name
@@ -13,7 +13,7 @@ def get_rrdgraph(rrd_file_name, data_source_name, start_time='N', end_time='N', 
     output_file = '/tmp/rrd_graph_{0}.png'.format(int(time.time() * 1000))
     def1 = DEF(rrdfile=rrd_file_name, vname=data_source_name, dsName=data_source_name)
     line1 = LINE(value=data_source_name, color=line_color, width=line_width)
-    g = Graph(output_file, start=start_time, end=end_time,
+    g = Graph(output_file, start=start_time, end=end_time, title=graph_title,
               lower_limit=0, imgformat='PNG', width=width, height=height)
     g.data.extend([def1, line1])
     g.write()
