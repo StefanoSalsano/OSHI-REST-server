73a937483a4061e885f3acf881cdb334096576b0
Marco Ferrari <ferrari.marco@gmail.com>
2015-12-10 19:57:29 +0100

    Updated Swagger documentation for rrdgraph endpoint.

--- oshi_rest_server/rrdgraph_server/views.py
+++ oshi_rest_server/rrdgraph_server/views.py
@@ -13,19 +13,40 @@ class RrdtoolViewSet(viewsets.ViewSet):
     @detail_route(url_path='rrdgraph', permission_classes=[AllowAny])
     def produce_rrdgraph(self, request, **kwargs):
         """
-            network_interface -- Network interface (port)
-            rrd_data_source -- RRD DS
-            time_scale -- Time Scale
-            graph_title -- Graph title
+            This endpoint generates a graph of an RRD using rrdgraph.
+            ---
+            parameters_strategy: replace
+            parameters:
+                - name: pk
+                  description: "RRD file name (including extension). Must be present in the
+                               rrdgraph_server.config.RRD_FILE_PATH directory"
+                  required: true
+                  type: string
+                  paramType: path
+                - name: rrd_data_source
+                  description: RRD data source. Must be defined in the specified RRD file
+                  required: true
+                  type: string
+                  paramType: query
+                - name: time_scale
+                  description: Graph time scale
+                  required: true
+                  type: string
+                  paramType: query
+                  allowMultiple: true
+                - name: graph_title
+                  description: Graph title
+                  required: false
+                  type: string
+                  paramType: query
         """
-        device = kwargs.get('pk')
-        network_interface = request.query_params.get('network_interface')
+        rrd_file_name = kwargs.get('pk')
         rrd_data_source = request.query_params.get('rrd_data_source')
         time_scale = request.query_params.get('time_scale')
         graph_title = request.query_params.get('graph_title')
 
         try:
-            rrd_graph_path = _generate_rrdgraph(device, network_interface, rrd_data_source, time_scale, graph_title)
+            rrd_graph_path = _generate_rrdgraph(rrd_file_name, rrd_data_source, time_scale, graph_title)
         except RrdGraphError, e:
             # Return appropriate status code on invalid requests
             return http.HttpResponse(status=e.status, content=e)
@@ -40,10 +61,10 @@ _RRDGRAPH_TIME_SCALES = {
 }
 
 
-def _generate_rrdgraph(device, network_interface, rrd_data_source, time_scale, graph_title):
+def _generate_rrdgraph(rrd_file_name, rrd_data_source, time_scale, graph_title):
     end = 'now'
     start = 'end' + _RRDGRAPH_TIME_SCALES[time_scale]
-    return get_rrdgraph(os.path.join(config.RRD_FILE_PATH, device + '-' + network_interface + '.rrd'), rrd_data_source,
+    return get_rrdgraph(os.path.join(config.RRD_FILE_PATH, rrd_file_name), rrd_data_source,
                         graph_title=graph_title, start_time=start, end_time=end)
 
 
